aaa5300f576ae6cc5065279724e5e975d793b00e
Constance Schmidt <coco390@gmail.com>
2024-06-07 10:45:44 -0400

    cleaned up CSS code

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from './footer.module.css'
-import logo from "../Images/Logo.svg";
+import logo from "../Images/logowhite.PNG";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faYoutube,
@@ -12,45 +12,50 @@ import { faEnvelope,
          faMap,
          faPhone, } from '@fortawesome/free-solid-svg-icons';
 
-const Footer = () => {
-    return(
-        <footer className={styles.footercontainer}>
-            <img src={logo} alt="Little Lemon Logo" />
-                    <div className={styles.footerNav}>
-                        <p className={styles.footerText}>Site Map</p>
-                         <a href="/" className={styles.footerItem}>Home</a>
-                         <a href="/" className={styles.footerItem}>About</a>
-                         <a href="/" className={styles.footerItem}>Menu</a>
-                         <a href="/" className={styles.footerItem}>Reservations</a>
-                         <a href="/" className={styles.footerItem}>Order Online</a>
-                         <a href="/" className={styles.footerItem}>Login</a>
-                    </div>
-                    <div className={styles.footerContact}>
-                        <p className={styles.footerHead}>Contact</p>
-                        <a href="/" className={styles.footerItem}>
-                        <FontAwesomeIcon icon={faMap}/>Address</a>
-                        <a href="/" className={styles.footerItem}>
-                        <FontAwesomeIcon icon={faPhone}/>Phone Number</a>
-                        <a href="mailto:customer@littlelemon.com" className={styles.footerItem} >
-                        <FontAwesomeIcon icon={faEnvelope}/>Email</a>
-                    </div>
-                <div className="social-container">
-                        <p className={styles.footerHead}>Connect With Us</p>
-                        <a href="https://www.facebook.com" className={`${styles.social} ${styles.facebook}`}>
-                            <FontAwesomeIcon icon={faFacebook} size="1x" />
-                        </a>
-                        <a href="https://www.x.com" className={`${styles.social} ${styles.instagram}`}>
-                            <FontAwesomeIcon icon={faTwitter} size="1x" />
-                        </a>
-                        <a href="https://www.instagram.com" className={`${styles.social} ${styles.instagram}`}>
-                            <FontAwesomeIcon icon={faInstagram} size="1x" />
-                        </a>
-                        <a href="https://www.youtube.com" className={`${styles.social} ${styles.youtube}`}>
-                            <FontAwesomeIcon icon={faYoutube} size="1x" />
-                        </a>
-                 </div>
-        </footer>
-    );
-}
-
+         const Footer = () => {
+            return (
+                <footer className={styles.footerContainer}>
+                        <div className={styles.footerlogo}>
+                             <img src={logo} alt="Little Lemon Logo" />
+                         </div>
+                        <div className={styles.footerNav}>
+                            <p className={styles.footerHead}>Site Map</p>
+                            <a href="/" className={styles.footerItem}>Home</a>
+                            <a href="/about" className={styles.footerItem}>About</a>
+                            <a href="/menu" className={styles.footerItem}>Menu</a>
+                            <a href="/reservations" className={styles.footerItem}>Reservations</a>
+                            <a href="/order-online" className={styles.footerItem}>Order Online</a>
+                            <a href="/login" className={styles.footerItem}>Login</a>
+                        </div>
+                        <div className={styles.footerContact}>
+                            <p className={styles.footerHead}>Contact</p>
+                            <a href="/" className={styles.footerItem}>
+                                <FontAwesomeIcon icon={faMap}  />123 Lemon, Chicago, IL 60611
+                            </a>
+                            <a href="/" className={styles.footerItem}>
+                                <FontAwesomeIcon icon={faPhone} /> (888)867-5309
+                            </a>
+                            <a href="mailto:customer@littlelemon.com" className={styles.footerItem}>
+                                <FontAwesomeIcon icon={faEnvelope} /> customer@littlelemon.com
+                            </a>
+                        </div>
+                        <div className={styles.footerSocial}>
+                            <p className={styles.footerHead}>Connect With Us</p>
+                            <a href="https://www.facebook.com" className={`${styles.social} ${styles.facebook}`}>
+                                <FontAwesomeIcon icon={faFacebook} />
+                            </a>
+                            <a href="https://www.twitter.com" className={`${styles.social} ${styles.twitter}`}>
+                                <FontAwesomeIcon icon={faTwitter}/>
+                            </a>
+                            <a href="https://www.instagram.com" className={`${styles.social} ${styles.instagram}`}>
+                                <FontAwesomeIcon icon={faInstagram} />
+                            </a>
+                            <a href="https://www.youtube.com" className={`${styles.social} ${styles.youtube}`}>
+                                <FontAwesomeIcon icon={faYoutube} />
+                            </a>
+                        </div>
+                </footer>
+            );
+        }
+        
 export default Footer;
