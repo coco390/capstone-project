5b4bf35e5e4e9b3fbab56639f417377eeb02949e
Constance Schmidt <coco390@gmail.com>
2024-06-12 08:17:00 -0400

    Fixed why my hamburger menu wasn't displaying

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -17,14 +17,14 @@ const Header = () => {
         <button className={styles.hamburger} onClick={toggleMenu}>
         {isOpen ? <FaTimes /> : <FaBars />}
       </button>
-      <nav className={styles.navContainer}>
+        <nav className={`${styles.navContainer} ${isOpen ? styles.open : ''}`}>
         <div className={styles.navLinks}>
-          <a href="/" className={styles.navItem}>HOME</a>
-          <a href="/about" className={styles.navItem}>ABOUT</a>
-          <a href="/menu" className={styles.navItem}>MENU</a>
-          <a href="/reservations" className={styles.navItem}>RESERVATIONS</a>
-          <a href="/order-online" className={styles.navItem}>ONLINE ORDERS</a>
-          <a href="/login" className={styles.navItem}>LOGIN</a>
+          <a href="/" className={styles.navItem}>Home</a>
+          <a href="/about" className={styles.navItem}>About</a>
+          <a href="/menu" className={styles.navItem}>Menu</a>
+          <a href="/reservations" className={styles.navItem}>Reservations</a>
+          <a href="/order-online" className={styles.navItem}>Online Orders</a>
+          <a href="/login" className={styles.navItem}>Login</a>
         </div>
       </nav>
     </header>
