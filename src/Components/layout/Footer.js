46d8af9e115b9217b2722af98973ff1d8458696d
Constance Schmidt <coco390@gmail.com>
2024-06-15 16:38:59 -0400

    Updated React Footer

--- src/Components/layout/Footer.js
+++ src/Components/layout/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
-import styles from "./footer.module.css"
+import { Link } from 'react-router-dom';
+import styles from "./footer.module.css";
 import logo from "../layout/logowhite.PNG";
+import pages from "../utilities/pages";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faYoutube,
@@ -12,42 +14,65 @@ import { faEnvelope,
          faMapMarkerAlt,
          faPhone, } from '@fortawesome/free-solid-svg-icons';
 
+const contacts = [
+    {icon: faMapMarkerAlt, info: "123 Ave, Chicago, IL 60611", },
+    {icon: faPhone, info: "(123) 456-7890", },
+    {icon: faEnvelope, info: "customer@littlelemon.com", },
+];
+
+const socials = [
+    { icon: faFacebook, name: 'facebook', },
+    { icon: faTwitter, name: 'twitter', },
+    { icon: faInstagram, name: 'instagram', },
+    { icon: faYoutube, name: 'youtube', },
+  ];
+
+const navLinks = Array.from(pages.values()).filter(page => page.anchorable);
+
 const Footer = () => {
     return (
         <footer className={styles.footerContainer}>
-            <div className={styles.footerlogo}>
-                <img src={logo} alt="Little Lemon Logo" />
+            <div className={styles.grid}>
+                <img className={styles.footerimg}src={logo} alt="Little Lemon Logo" />
             </div>
             <nav className={styles.footerNav}>
-                <p className={styles.footerHead}>Site Map</p>
-                <ul className={styles.footerList}>
-                    <li><a href="/" className={styles.footerItem}>Home</a></li>
-                    <li><a href="/about" className={styles.footerItem}>About</a></li>
-                    <li><a href="/menu" className={styles.footerItem}>Menu</a></li>
-                    <li><a href="/reservations" className={styles.footerItem}>Reservations</a></li>
-                    <li><a href="/order-online" className={styles.footerItem}>Order Online</a></li>
-                    <li><a href="/login" className={styles.footerItem}>Login</a></li>
+                <h4>Site Map</h4>
+                <ul>
+                    {navLinks.map((navLink, index) =>
+                        <li key={index}>
+                         <Link to={navLink.path}>{navLink.name}</Link>
+                         </li>
+                    )}
                 </ul>
             </nav>
-            <address iv className={styles.footerContact}>
-                <p className={styles.footerHead}>Contact Us</p>
+            <div className={styles.footerContact}>
+                <h4>Contact Us</h4>
                 <ul className={styles.footerList}>
-                    <li><a href="/" className={styles.footerItem}><FontAwesomeIcon icon={faMapMarkerAlt} />123 Lemon, Chicago, IL 60611</a></li>
-                    <li><a href="/" className={styles.footerItem}><FontAwesomeIcon icon={faPhone} /> (888)123-4567</a></li>
-                    <li><a href="mailto:customer@littlelemon.com" className={styles.footerItem}><FontAwesomeIcon icon={faEnvelope} /> customer@littlelemon.com</a></li>
+                    <address>
+                    {contacts.map((contact, index) =>
+                        <p key={index}>
+                         <FontAwesomeIcon icon={contact.icon} /> {contact.info}
+                        </p>
+                    )}
+                    </address>
                 </ul>
-            </address>
-            <div className={styles.footerSocial}>
-                <p className={styles.footerHead}>Connect With Us</p>
-                <ul className={styles.footerMedia}>
-                    <li><a href="https://www.facebook.com" className={`${styles.social} ${styles.facebook}`}><FontAwesomeIcon icon={faFacebook} /></a></li>
-                    <li><a href="https://www.twitter.com" className={`${styles.social} ${styles.twitter}`}><FontAwesomeIcon icon={faTwitter} /></a></li>
-                    <li><a href="https://www.instagram.com" className={`${styles.social} ${styles.instagram}`}><FontAwesomeIcon icon={faInstagram} /></a></li>
-                    <li><a href="https://www.youtube.com" className={`${styles.social} ${styles.youtube}`}><FontAwesomeIcon icon={faYoutube} /></a></li>
-                </ul>
+
+                <div className={styles.footerSocial}>
+                    <h4>Connect With Us</h4>
+                    {socials.map((social, index) =>
+                    <a
+                        key={index}
+                        href={`https://www.${social.name}.com`}
+                        target="_blank"
+                        rel="noreferrer"
+                            >
+                        <FontAwesomeIcon icon={social.icon} size="lg" />
+                     </a>
+                     )}
+                </div>
             </div>
         </footer>
     );
-}
+};
 
 export default Footer;
