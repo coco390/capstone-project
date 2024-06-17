1fdffec8c95e27fba500586ad0f629d92e3a744c
Constance Schmidt <coco390@gmail.com>
2024-06-17 11:43:55 -0400

    deleted unnecessary file

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './App';
-import ScrollToTop from './Components/layout/ScrollToTop';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
